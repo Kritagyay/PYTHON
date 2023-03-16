cf81db88147131c50b1d626eb98a940d4ceec824
Kritagyay <104650199+VenomKJ@users.noreply.github.com>
2023-03-16 14:57:23 +0530

    date time module

---
+++ Birthday+Wisher/Birthday Wisher/date_time.py
@@ -0,0 +1,10 @@
+import datetime as dt
+now=dt.datetime.now()
+print(now)
+
+year=now.year
+month=now.month
+current_day=now.weekday()
+print(current_day)
+d_o_b=dt.datetime(year=2004,month=4,day=4)
+print(d_o_b)
